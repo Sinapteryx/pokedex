40f9ae551db551411d74aed8bf0c0d58f8206306
Donglin Yu <yu.jerry@msn.com>
2024-09-25 10:21:53 +1200

    Display Detailed Pokémon Information

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
 
   useEffect(() => {
-    // Fetch the first 151 Pokémon
     axios
       .get('https://pokeapi.co/api/v2/pokemon?limit=151')
       .then((response) => {
@@ -16,16 +16,41 @@ function App() {
       });
   }, []);
 
+  const fetchPokemonDetails = (url) => {
+    axios
+      .get(url)
+      .then((response) => {
+        setSelectedPokemon(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokémon details:', error);
+      });
+  };
+
   return (
     <div className="App">
       <h1>Pokedex</h1>
       <ul>
         {pokemonList.map((pokemon, index) => (
-          <li key={index}>
+          <li key={index} onClick={() => fetchPokemonDetails(pokemon.url)}>
             #{index + 1} {pokemon.name}
           </li>
         ))}
       </ul>
+
+      {selectedPokemon && (
+        <div>
+          <h2>{selectedPokemon.name}</h2>
+          <p>Height: {selectedPokemon.height}</p>
+          <p>Weight: {selectedPokemon.weight}</p>
+          <p>Types:</p>
+          <ul>
+            {selectedPokemon.types.map((typeInfo, index) => (
+              <li key={index}>{typeInfo.type.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
